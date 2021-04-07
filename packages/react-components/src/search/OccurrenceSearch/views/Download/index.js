a03b55b46573e876b312f9b128e81e0306d47eb3
Morten Høfft <mhoefft@gbif.org>
2021-04-07 15:03:13 +0200

    show error if invalid download query

--- packages/react-components/src/search/OccurrenceSearch/views/Download/index.js
+++ packages/react-components/src/search/OccurrenceSearch/views/Download/index.js
@@ -5,9 +5,9 @@ import { useQuery } from '../../../../dataManagement/api';
 import { filter2predicate } from '../../../../dataManagement/filterAdapter';
 
 const DOWNLOAD = `
-query table($predicate: Predicate){
+query($predicate: Predicate){
   occurrenceSearch(predicate: $predicate, size: 0) {
-    _predicate
+    _downloadPredicate
   }
 }
 `;
@@ -16,7 +16,6 @@ function Download() {
   const currentFilterContext = useContext(FilterContext);
   const { rootPredicate, predicateConfig } = useContext(OccurrenceContext);
   const { data, error, loading, load } = useQuery(DOWNLOAD, { lazyLoad: true, keepDataWhileLoading: true });
-  const [fullPredicate, setPredicate] = useState();
 
   useEffect(() => {
     const predicate = {
@@ -27,14 +26,18 @@ function Download() {
       ].filter(x => x)
     }
     load({ variables: { predicate } });
-    setPredicate(predicate);
   }, [currentFilterContext.filterHash, rootPredicate]);
+
+  const fullPredicate = data?.occurrenceSearch?._downloadPredicate?.predicate;
+  const err = data?.occurrenceSearch?._downloadPredicate?.err;
 
   return <div>
       You are about to download, to do so you will be redirected to GBIF.org. Be aware that an account is needed to download the content.
+      {err && <div>
+          {err.message}
+        </div>}
       {fullPredicate && <>
-        <pre>{JSON.stringify(fullPredicate)}</pre>
-        <a href={`https://www.gbif-uat.org/occurrence/download/request?predicate=${encodeURIComponent(JSON.stringify(fullPredicate))}#create`}>Download</a>
+        <a href={`https://www.gbif.org/occurrence/download/request?predicate=${encodeURIComponent(JSON.stringify(fullPredicate))}#create`}>Download</a>
       </>}
     </div>
 }
